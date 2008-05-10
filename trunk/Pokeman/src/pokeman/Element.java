daf66c12602eabcccf911c465bff363ee9c79730
jukeboxlord <jukeboxlord@c28df673-d54c-0410-adb8-4fbafd67059f>
2008-05-10 18:30:55 +0000

    Currently broken ELement, but uploaded for reference

---
+++ trunk/Pokeman/src/pokeman/Element.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pokeman;
+
+/**
+ *
+ * @author Kunal
+ */
+public enum Element {
+    FIRE({Element.GRASS, Element.ICE, Element.BUG},
+        {Element.FIRE, Element.WATER, Element.GROUND, Element.ROCK}, {} ),
+    WATER    ({),
+    GROUND   (),
+    FLYING   (),
+    POISON   (),
+    NORMAL   (),
+    PSYCHIC  (),
+    GHOST    (),
+    BUG      (),
+    ROCK     (),
+    GRASS    (),
+    FIGHTING (),
+    ELECTRIC (),
+    ICE      (),
+    DRAGON   ();
+            
+    private final Element[] strongAgainst;
+    private final Element[] weakAgainst;
+    private final Element[] cantHurt;
+    
+    Element(Element[] strongAgainst, Element[] weakAgainst,
+        Element[] cantHurt){
+        this.strongAgainst = strongAgainst;
+        this.weakAgainst = weakAgainst;
+        this.cantHurt = cantHurt;
+    }
+        
+}
