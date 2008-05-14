011c9ab4165ca13fe37369a3c6808bc82522641e
mark.d.benjamin <mark.d.benjamin@comcast.net>
2008-05-14 00:49:38 +0000

    Update to the window class, now it doesn't get angry when there isn't a level to load, it just draws a black screen. I started on person but didn't have time to get far

---
+++ Pokeman/src/pokeman/Person.java
@@ -0,0 +1,33 @@
+package pokeman;
+
+import java.awt.Graphics2D;
+
+
+public class Person
+{
+    private String name,speech;
+    private int x,y,direction;
+    private Animation walk;
+
+    /**
+     * Constructor for objects of class Person
+     */
+    public Person(String n,String s,int x,int y)
+    {
+        speech = s;
+        name = n;
+        this.x = x;
+        this.y = y;
+        walk = new Animation(name);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void draw(Graphics2D g){
+        g.drawImage(walk.getFrame(),null,x,y);
+        walk.nextFrame(direction);
+    }
+}
