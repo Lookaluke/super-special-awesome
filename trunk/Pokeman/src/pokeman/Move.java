bae1c10c04986042615f3f271ba23aa489eac384
mark.d.benjamin@comcast.net <mark.d.benjamin@comcast.net@c28df673-d54c-0410-adb8-4fbafd67059f>
2008-05-19 21:56:28 +0000

    Some work on the pokemon class and the move class will come soon

--- trunk/Pokeman/src/pokeman/Move.java
+++ trunk/Pokeman/src/pokeman/Move.java
@@ -10,16 +10,39 @@ package pokeman;
  * @author Kunal
  */
 public class Move {
+    
+    public static final int HP = 0,ATTACK=1,DEFENSE=2,SPECIAL=3,SPEED=4,ACCURACY=5,EVASION=6;
+    
     private Element elem;
     private int power;
     private String name;
     private Animation anim;
     private int accuracy;
-    private String description;
     private int totalPP, currentPP;
     private Status effect;
     private int statusPercentage;
+    private int attacksWhat;
     
+    public Move(String n,Element e,int p,int what,Animation a,int ac,int tot,int cur,Status ef,int percent){
+        elem = e;
+        power = p;
+        attacksWhat = what;
+        n = name;
+        anim = a;
+        accuracy = ac;
+        totalPP = tot;
+        currentPP = cur;
+        effect = ef;
+        statusPercentage = percent;
+    }
+    
+    /**
+     * Makes a move from a file
+     * @param f The name of the directory with the move
+     */
+    public Move(String f){
+        
+    }
 	
     public Element getElement()
     {
@@ -29,6 +52,10 @@ public class Move {
     public int power()
     {
         return power;
+    }
+    
+    public String name(){
+        return name;
     }
 	
     public void draw(){
