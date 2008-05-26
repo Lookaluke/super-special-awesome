4af7dc785e12c546f6b356bda28c2e17e6310dc3
hpatel516 <hpatel516@c28df673-d54c-0410-adb8-4fbafd67059f>
2008-05-26 17:07:55 +0000

    Added some methods that may help and fixed enemy pokemon PP issue

--- trunk/Pokeman/src/pokeman/Battle.java
+++ trunk/Pokeman/src/pokeman/Battle.java
@@ -21,7 +21,10 @@ public class Battle {
     
     public Battle(Character you, Pokemon enemy)
     {
-        
+        yours = you.currentPokemon()[0];
+        theirs = new Pokemon[1];
+        theirs[0] = enemy;
+        theirCurrent = enemy;
     }
     
     public Battle(Character you, Trainer enemy){
@@ -35,22 +38,35 @@ public class Battle {
         //you check if dead. if not, then do the second move
         //check if dead again.
         
-        int movenumber = (int)(Math.random() * 4);
-        Move theirMove = theirCurrent.getMoves()[movenumber];
+
+
+        int[] movesLeft = theirCurrent.getMovesLeft();
+               
+        int movenumber = (int)(Math.random() * movesLeft.length);
+        int moveChoice = movesLeft[movenumber];
+        Move theirMove = theirCurrent.getMoves()[moveChoice]; 
         
-        if (yours.getSpeed() > theirCurrent.getSpeed()){
+        if (yours.getSpeed() > theirCurrent.getSpeed())
+        {
             //perform your move first
             //reduce pp only if its a human...
             
             //we need to modify takeDamage() to properly handle pokemon fainting
-            theirCurrent.takeDamage(calculateDamage(yourMove, true));
-            
-            
+            if(yourMove.useMove())
+            {
+                theirCurrent.takeDamage(calculateDamage(yourMove, true));
+            }
+            else
+            {
+                //tell player that there is no pp left, use different move                
+            }
+           
             //now do their move
             yours.takeDamage(calculateDamage(theirMove, false));
-        } else {
+        } 
+        else 
+        {
             yours.takeDamage(calculateDamage(theirMove, false));
-            
             theirCurrent.takeDamage(calculateDamage(yourMove, true));
         }
     }
@@ -76,23 +92,27 @@ public class Battle {
         
         int accuracything = (int) (attacker.getAccuracy() * m.accuracy() * 0.0256);
             
-        if ((int)(Math.random() * 256) < accuracything){
+        if ((int)(Math.random() * 256) < accuracything)
+        {
             //move hit, now calculate damage
             int level = attacker.getLevel();
             int attack = attacker.getAttack();
             int power = m.power();
             int defense = defender.getDefense();
             double stab;
-            if (m.element() == attacker.getElement1() || m.element() == attacker.getElement2()){
+            if (m.element() == attacker.getElement1() || m.element() == attacker.getElement2())
+            {
                 stab = 1.5;
-            } else {
+            }
+            else 
+            {
                 stab = 1;
             }
             double typeModifer = m.element().multiplerAgainst(defender.getElement1()) * m.element().multiplerAgainst(defender.getElement2()) * 10;
             int randomNumber = (int)(Math.random() * 39) + 217;
             return (int)(((((Math.min(((((2*level/5 + 2)*attack*power)/Math.max(1, defense))/50), 997) + 2)*stab)*typeModifer)/10)*randomNumber)/255;
         } else {
-            return 0;
+            return 0;// return -1?
         }
     }
 }
